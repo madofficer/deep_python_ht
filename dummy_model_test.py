0e0d112c8022d70e1508c1f64e5555f9ba7bb4f5
Borsch <l.boristag@gmail.com>
2024-09-20 13:48:29 +0300

    model coverage > 90

--- dummy_model_test.py
+++ dummy_model_test.py
@@ -31,10 +31,22 @@ class TestModel(unittest.TestCase):
     def test_predict_message_mood_invalid_thresholds_type(self, mock_prediction):
         mock_prediction.return_value = 0.5
         with self.assertRaises(TypeError):
-            predict_message_mood('Чапаев и пустота', 0, 11)
+            predict_message_mood('Чапаев и пустота', 0, 0.9)
 
     @patch('dummy_model.DummyModel.predict')
     def test_predict_message_invalid_thresholds_value(self, mock_prediction):
         mock_prediction.return_value = 0.5
         with self.assertRaises(ValueError):
             predict_message_mood("Вулкан", 0.5, 0.4)
+
+    @patch('dummy_model.DummyModel.predict')
+    def test_predict_message_invalid_good_thresholds_value(self, mock_prediction):
+        mock_prediction.return_value = 0.5
+        with self.assertRaises(ValueError):
+            predict_message_mood("Вулкан", 0.5, 1.1)
+
+    @patch('dummy_model.DummyModel.predict')
+    def test_predict_message_invalid_bad_thresholds_value(self, mock_prediction):
+        mock_prediction.return_value = 0.5
+        with self.assertRaises(ValueError):
+            predict_message_mood("Вулкан", -0.1, 0.77)
