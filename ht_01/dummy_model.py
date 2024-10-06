2db9aec21f02390f89e54941f529d5f56d7b4946
Borsch <l.boristag@gmail.com>
2024-10-06 22:15:32 +0300

    fixed pipeline

--- ht_01/dummy_model.py
+++ ht_01/dummy_model.py
@@ -22,7 +22,7 @@ def predict_message_mood(
             f" not {type(good_thresholds).__name__}"
         )
 
-    if not (0 < bad_thresholds < 1):
+    if not 0 < bad_thresholds < 1:
         raise ValueError(
             f"Argument 'bad_thresholds' must be between 0 and 1,"
             f" got {bad_thresholds}"
@@ -33,7 +33,7 @@ def predict_message_mood(
             f" 'bad_thresholds', got 'bad_thresholds'={bad_thresholds},"
             f"'good_thresholds'={good_thresholds}"
         )
-    if not (0 < good_thresholds < 1):
+    if not 0 < good_thresholds < 1:
         raise ValueError(
             f"Argument 'good_thresholds' must be between 0 and 1,"
             f" got {good_thresholds}"
