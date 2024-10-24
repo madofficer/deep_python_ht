32afcb534ff58d3e9d56ffb024f815443384d75a
Borsch <l.boristag@gmail.com>
2024-10-25 00:49:52 +0300

    ht5 tests, readme, wf

---
+++ 05/lru_cache.py
@@ -0,0 +1,58 @@
+from collections.abc import Hashable
+
+from typing_extensions import Optional
+
+
+class DLLNode:
+    def __init__(self, key: Hashable, val: Optional[any]):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int = 2):
+        self._capacity = capacity
+        self._cache = {}
+        self._left, self._right = DLLNode(0, 0), DLLNode(0, 0)
+        self._left.next, self._right.prev = self._right, self._left
+
+    def _remove(self, node: DLLNode) -> None:
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def _insert(self, node: DLLNode) -> None:
+        prev, nxt = self._right.prev, self._right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
+
+    def get(self, key: Hashable) -> any:
+        if key in self._cache:
+            self._remove(self._cache[key])
+            self._insert(self._cache[key])
+            return self._cache[key].val
+        return -1
+
+    def set(self, key: Hashable, val: any) -> None:
+        if key in self._cache:
+            self._remove(self._cache[key])
+        self._cache[key] = DLLNode(key, val)
+        self._insert(self._cache[key])
+
+        if len(self._cache) > self._capacity:
+
+            lru = self._left.next
+            self._remove(lru)
+            del self._cache[lru.key]
+
+
+# if __name__ == "__main__":
+#     obj = LRUCache(2)
+#     param_1 = obj.get(1)
+#     print(param_1)
+#     obj.set(1, 1)
+#     print(obj.get(1))
+#     obj.set('1', 'str')
+#     obj.set((1, 2), 'val2')
+#     print(obj.get(1))
+#     print(obj.get('1'))
+#     print(obj.get((1, 2)))
--- 05/test_lru_cache.py
+++ 05/test_lru_cache.py
@@ -1,10 +1,106 @@
 import unittest
+from functools import lru_cache
+
+from lru_cache import DLLNode, LRUCache
 
 
 class TestLRUCache(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def setUp(self):
+        self.cache = LRUCache(2)
+
+    def test_init(self):
+        self.assertEqual(self.cache._capacity, 2)
+        self.assertEqual(self.cache._cache, {})
+
+    def test_set_get(self):
+        self.cache.set(1, 1)
+        res = self.cache.get(1)
+        self.assertEqual(res, 1)
+
+        self.cache.set((1, 2, 3), 2)
+        res = self.cache.get((1, 2, 3))
+        self.assertEqual(res, 2)
+
+        res = self.cache.get(100)
+        self.assertEqual(res, -1)
+
+    def test_set_existing_key(self):
+        self.cache.set(1, 100)
+        res = self.cache.get(1)
+        self.assertEqual(res, 100)
+
+        self.cache.set((1, 2, 3), 77)
+        res = self.cache.get((1, 2, 3))
+        self.assertEqual(res, 77)
+
+    def test_eviction(self):
+        self.cache.set(1, 1)
+        self.cache.set("2", "val2")
+        self.cache.set(3.0, {3})
+        res1 = self.cache.get(1)
+        res2 = self.cache.get("2")
+        res3 = self.cache.get(3.0)
+        self.assertEqual(res1, -1)
+        self.assertEqual(res2, "val2")
+        self.assertEqual(res3, {3})
+
+    def test_eviction_order(self):
+        self.cache.set(1, 10)
+        self.cache.set(2, 22)
+        res1 = self.cache.get(1)
+        self.assertEqual(res1, 10)
+        self.cache.set("3", "_3_")
+        res2 = self.cache.get(2)
+        self.assertEqual(res2, -1)
+        res3 = self.cache.get("3")
+        self.assertEqual(res3, "_3_")
+
+    def test_invalid_key(self):
+        self.cache.set(1, 11)
+        self.cache.set(5, "55")
+
+        with self.assertRaises(TypeError):
+            self.cache.set([1], 123)
+        with self.assertRaises(TypeError):
+            self.cache.set({33}, 321)
+
+        res1 = self.cache.get(1)
+        res2 = self.cache.get(5)
+        self.assertEqual(res1, 11)
+        self.assertEqual(res2, "55")
+
+    def test_big_cache(self):
+        big_cache = LRUCache(60)
+
+        for key in range(60):
+            big_cache.set(f"{key=}", key)
+
+        for key in range(40, 100):
+            big_cache.set(f"{key=}", key)
+
+        res1 = big_cache.get("key=0")
+        self.assertEqual(res1, -1)
+        res2 = big_cache.get("key=39")
+        self.assertEqual(res2, -1)
+        res3 = big_cache.get("key=40")
+        self.assertEqual(res3, 40)
+        big_cache.set("new_key", 41)
+        res4 = big_cache.get("key=41")
+        self.assertEqual(res4, -1)
+        res5 = big_cache.get("key=40")
+        self.assertEqual(res5, 40)
+
+    def test_non_pos_init(self):
+        non_pos_obj = LRUCache(0)
+        non_pos_obj.set(1, 1)
+        res1 = non_pos_obj.get(1)
+        self.assertEqual(res1, -1)
+
+        non_pos_obj = LRUCache(-1)
+        non_pos_obj.set(-1, -1)
+        res2 = non_pos_obj.get(-1)
+        self.assertEqual(res2, -1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
